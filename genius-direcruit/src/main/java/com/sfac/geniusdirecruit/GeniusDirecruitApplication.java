85005d15b9414174822aad60cc78b813df6abe63
HymanHu <hujiangyx@163.com>
2021-01-20 20:47:16 +0800

    git test

--- genius-direcruit/src/main/java/com/sfac/geniusdirecruit/GeniusDirecruitApplication.java
+++ genius-direcruit/src/main/java/com/sfac/geniusdirecruit/GeniusDirecruitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GeniusDirecruitApplication {
 
     public static void main(String[] args) {
+		
         SpringApplication.run(GeniusDirecruitApplication.class, args);
     }
 
