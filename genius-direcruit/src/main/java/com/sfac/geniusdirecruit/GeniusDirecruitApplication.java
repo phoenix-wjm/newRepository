fe0f5ba82701f69b36baf68023074de20d771c6e
‘swj-br’ <1424358993@qq.com>
2021-01-21 08:54:33 +0800

    git test2

--- genius-direcruit/src/main/java/com/sfac/geniusdirecruit/GeniusDirecruitApplication.java
+++ genius-direcruit/src/main/java/com/sfac/geniusdirecruit/GeniusDirecruitApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GeniusDirecruitApplication {
 
-	// test
+	// tests----
     public static void main(String[] args) {
 		
         SpringApplication.run(GeniusDirecruitApplication.class, args);
